1d71a4c6c29f066f33f0ce76aee9cf9221235fb3
Levi Zitting <levizitting5663@gmail.com>
2017-06-23 14:48:17 -0500

    Adding basic setup for frontend dev

---
+++ frontend/gulpfile.js
@@ -0,0 +1,89 @@
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var pug = require("gulp-pug");
+var ts = require("gulp-typescript");
+var browserify = require("browserify");
+var browserSync = require("browser-sync").create();
+
+paths = {
+	node: "./node_modules/",
+	public: "./public/",
+	source: "./src/", 
+}
+
+paths.jquery = paths.node + "jquery/dist/jquery.min.js";
+paths.bootstrapCSS = paths.node + "bootstrap/dist/css/bootstrap.min.css";
+paths.bootstrapJs = paths.node + "bootstrap/dist/js/bootstrap.min.js";
+paths.js = paths.public + "js/";
+paths.css = paths.public + "css/";
+paths.sass = paths.source + "sass/";
+paths.pug = paths.source + "pug/";
+paths.typscript = paths.source + "typescript/";
+
+
+gulp.task("copy", function() {
+	// Copy css files to public folder
+	gulp.src(paths.bootstrapCSS)
+		.pipe(gulp.dest(paths.css));
+	// Copy JS files to public folder
+	gulp.src([paths.jquery, paths.bootstrapJs])
+		.pipe(gulp.dest(paths.js));
+});
+
+// Compile sass code
+gulp.task("sass", function() {
+	gulp.src(paths.sass + "*.scss")
+		.pipe(sass({
+			outputStyle: "expanded"
+		}).on("error", sass.logError))
+		.pipe(gulp.dest(paths.css));
+});
+
+// Compile sass code for production
+gulp.task("sass-production", function() {
+	gulp.src(paths.sass + "*.scss")
+		.pipe(sass({
+			outputStyle: "compressed"
+		}).on("error", sass.logError))
+		.pipe(gulp.dest(paths.css));
+});
+
+// Compile typescript code
+gulp.task("typescript", function() {
+	gulp.src([paths.typscript + "**/*.ts", "!" + paths.typscript + "**/_*.ts"])
+		.pipe(ts({
+			target: "ES5"
+		}))
+		.pipe(gulp.dest(paths.js));
+});
+
+gulp.task("pug", function() {
+	gulp.src([paths.pug + "**/*.pug", "!" + paths.pug + "**/_*.pug"])
+		.pipe(pug({
+			pretty: true
+		}).on('error', function (e) {
+			console.log(e);
+		}))
+		.pipe(gulp.dest(paths.public));
+});
+
+gulp.task('watch',function() {
+		gulp.start(["pug"], ["sass"], ["typescript"]);
+		browserSync.init({
+			open: "external",
+			port: 8080,
+			server: "public/",
+			logFileChanges: true
+		});
+    gulp.watch(paths.pug + "**/*", ["pug"]);
+    gulp.watch(paths.sass + "**/*", ["sass"]);
+    gulp.watch(paths.typscript + "**/*", ["typescript"]);
+    gulp.watch(paths.source + "**/*").on("change", function() {
+			// Delay browser reload by 1 second
+			setTimeout(function () {
+				browserSync.reload()
+			}, 1000)
+		});
+});
+
+gulp.task("default", ["watch"]);
