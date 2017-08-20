d4bf727fc1b08902605965694e8e7392ca75c562
enoragon <enoragon@gmail.com>
2017-08-20 16:09:22 -0500

    Add Quality Control Pages

--- backend/resources/views/services/quality-control.blade.php
+++ backend/resources/views/services/quality-control.blade.php
@@ -1,25 +1,31 @@
 @extends('layouts.master')
 
 @section('content')
-    <div class="image-header valign-parent" id="quality control-header">
+    <div class="image-header valign-parent" id="quality-header">
         <div class="valign-child">
             <h1>Quality Control</h1>
         </div>
     </div>
-    <div class="container" id="detailing-panels">
-        <div class="row">
-            <div class="col-md-6">
+    <div class="container quality">
+        <div class= "quality-panels">
+
+            <div class="q-cell">
                 <div class="panel panel-default">
-                    <div class="panel-heading text-center">Tekla Software</div>
+                    <div class="panel-heading text-center">Standards</div>
                     <div class="panel-body">
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                        <p>Lorem ipsum dolor sit amet, ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                     </div>
                 </div>
             </div>
-            <div class="col-md-6">
+            <div class="q-cell">
+                <div class="center">
+                    <img src="http://via.placeholder.com/360x450">
+                </div>
+            </div>
+            <div class="q-cell">
                 <div class="panel panel-default">
-                    <div class="panel-heading text-center">Detailing</div>
+                    <div class="panel-heading text-center">Integrity</div>
                     <div class="panel-body">
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                         <p>
@@ -27,6 +33,25 @@
 
                         </p>
                     </div>
+                </div>
+            </div>
+            <div class="q-cell">
+                <div class="center">
+                    <img src="http://via.placeholder.com/350">
+                </div>
+            </div>
+            <div class="q-cell">
+                <div class="panel panel-default">
+                    <div class="panel-heading text-center">Finished Product</div>
+                    <div class="panel-body">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                        
+                    </div>
+                </div>
+            </div>
+            <div class="q-cell">
+                <div class="center">
+                    <img src="http://via.placeholder.com/350">
                 </div>
             </div>
         </div>
